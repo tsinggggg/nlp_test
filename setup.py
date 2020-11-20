e2c36bdc0cb55281f3b624a1f3361eb19d80722c
qc <c29qian@uwaterloo.ca>
2020-11-20 12:02:18 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+from __init__ import __version__
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='nlptest',
+    version=__version__,
+    packages=find_packages(exclude=['tests*',
+                                    'wandb*'
+                                    ]),
+    install_requirements=requirements,
+    description="NLP tests",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True
+)
